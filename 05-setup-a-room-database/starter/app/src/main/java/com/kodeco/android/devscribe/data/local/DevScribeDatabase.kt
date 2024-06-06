bdf1f083ad6427b423047cfa156a0d29eafe0b69
Harun Wangereka <harunwangereka@gmail.com>
2024-06-06 13:27:59 +0300

    fixes

--- 05-setup-a-room-database/starter/app/src/main/java/com/kodeco/android/devscribe/data/local/DevScribeDatabase.kt
+++ 05-setup-a-room-database/starter/app/src/main/java/com/kodeco/android/devscribe/data/local/DevScribeDatabase.kt
@@ -3,6 +3,6 @@ package com.kodeco.android.devscribe.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [], version = 1)
+@Database(version = 1)
 abstract class DevScribeDatabase: RoomDatabase() {
 }
